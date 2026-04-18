ff072daee068366834d42bd03312dd770bcea5c7
Shaveen08 <shaveenkumar0804@gmail.com>
2026-04-18 13:23:32 +0530

    Feat: roman to integer problem and solution added

--- arrays/01-two-sum.js
+++ arrays/01-two-sum.js
@@ -6,7 +6,8 @@ Description:
 Given an array of integers, return indices of two numbers such that they add up to a target.
 */
 
-function twoSum(nums, target) {
+// Method 1
+function twoSumMethod1(nums, target) {
   for (let i = 0; i < nums.length; i++) {
     for (let j = i + 1; j < nums.length; j++) {
       if (nums[i] + nums[j] === target) {
@@ -16,4 +17,17 @@ function twoSum(nums, target) {
   }
 }
 
-console.log(twoSum([2, 7, 11, 15], 9)); // [0, 1]
+console.log(twoSumMethod1([2, 7, 11, 15], 9)); // [0, 1]
+
+// Method 2
+function twoSumMethod2(nums, target){
+  for(let i = 0; i < nums.length; i++){
+    for(let j = i + 1; j < nums.length; j++){
+      if(nums[i] == target - nums[j]){
+        return [i ,j];
+      }
+    }
+  }
+}
+
+console.log(twoSumMethod2([2, 7, 11, 15], 9)); // [0, 1]
